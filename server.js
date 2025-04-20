7405609b6b8a177cbf06d6f5644f47649634237e
rbstokes22 <rbstokes22@gmail.com>
2025-04-19 23:59:26 -0400

    Started bitwise, in work.

--- server.js
+++ server.js
@@ -24,42 +24,39 @@ let clients = {};
 
 // This will be an enum in the server
 let CMDS = [null, // Used to index to a +1
-    "GET_ALL", "CALIBRATE_TIME"
+    "GET_ALL", "CALIBRATE_TIME", "NEW_LOG_RCVD", 
+    "RELAY_CTRL", "RELAY_TIMER", "ATTACH_RELAYS", 
+    "SET_TEMPHUM", "SET_SOIL", "SET_LIGHT", "SET_SPEC_INTEGRATION_TIME", "SET_SPEC_GAIN", 
+    "CLEAR_AVERAGES", "SEND_REPORT_SET_TIME", "SAVE_AND_RESTART", "GET_TRENDS"
 ];
 
-CMDS[16] = "ATTACH_TEMP_RELAY";
-CMDS[17] = "SET_TEMP_RE_LWR_THAN";
-CMDS[18] = "SET_TEMP_RE_GTR_THAN";
-CMDS[19] = "SET_TEMP_RE_COND_NONE";
-CMDS[20] = "SET_TEMP_ALT_LWR_THAN";
-CMDS[21] = "SET_TEMP_ALT_GTR_THAN";
-CMDS[22] = "SET_TEMP_ALT_COND_NONE";
-CMDS[51] = "GET_TRENDS"; // Add this special to test.
-
-let allData = {"firmv": "1.0.0","id": "0","newLog": 0,"sysTime": 62435,
-    "hhmmss": "17:20:35","timeCalib": 1,"re1": 0,"re1TimerEn": 0,
-    "re1TimerOn": 99999,"re1TimerOff": 99999,"re2": 0,"re2TimerEn": 0,
-    "re2TimerOn": 99999,"re2TimerOff": 99999,"re3": 0,"re3TimerEn": 0,
-    "re3TimerOn": 99999,"re3TimerOff": 99999,"re4": 0,"re4TimerEn": 0,
-    "re4TimerOn": 99999,"re4TimerOff": 99999,"temp": 26.5,"tempRe": 2,
-    "tempReCond": 1,"tempReVal": 1020,"tempAltCond": 2,"tempAltVal": 1150,
-    "hum": 58.65,"humRe": 255,"humReCond": 2,"humReVal": 0,"humAltCond": 2,
-    "humAltVal": 0,"SHTUp": 1,"tempAvg": 21.71,"humAvg": 45.87,
-    "tempAvgPrev": 0,"humAvgPrev": 0,"soil1": 2633,"soil1AltCond": 2,
-    "soil1AltVal": 0,"soil1Up": 1,"soil2": 182,"soil2AltCond": 2,
-    "soil2AltVal": 0,"soil2Up": 1,"soil3": 177,"soil3AltCond": 2,"soil3AltVal": 0,
-    "soil3Up": 1,"soil4": 0,"soil4AltCond": 2,"soil4AltVal": 0,"soil4Up": 1,
-    "violet": 255,"indigo": 527,"blue": 942,"cyan": 1375,"green": 2375,
-    "yellow": 4091,"orange": 5640,"red": 3851,"nir": 1534,"clear": 15585,
-    "photo": 3120,"violetAvg": 98.41,"indigoAvg": 202.22,"blueAvg": 356.18,
-    "cyanAvg": 530.24,"greenAvg": 906.52,"yellowAvg": 1568.42,
-    "orangeAvg": 2168.73,"redAvg": 1460.59,"nirAvg": 570.13,"clearAvg": 5700.67,
-    "photoAvg": 1148.59,"violetAvgPrev": 0,"indigoAvgPrev": 0,
-    "blueAvgPrev": 0,"cyanAvgPrev": 0,"greenAvgPrev": 0,"yellowAvgPrev": 0,
-    "orangeAvgPrev": 0,"redAvgPrev": 0,"nirAvgPrev": 0,"clearAvgPrev": 0,
-    "photoAvgPrev": 0,"lightRe": 255,"lightReCond": 2,"lightReVal": 0,
-    "lightDur": 6508,"photoUp": 1,"specUp": 1,"darkVal": 500,
-    "repTimeEn": 0,"repSendTime": 86340};
+let allData = {
+    "firmv": "1.0.0", "id": "1", "newLog": 0, "sysTime": 13683, "hhmmss": "3:48:3",
+    "timeCalib": 0, "re1": 0, "re1TimerEn": 0, "re1TimerOn": 99999,
+    "re1TimerOff": 99999, "re2": 0, "re2TimerEn": 0, "re2TimerOn": 99999,
+    "re2TimerOff": 99999, "re3": 0, "re3TimerEn": 0, "re3TimerOn": 99999,
+    "re3TimerOff": 99999, "re4": 0, "re4TimerEn": 0, "re4TimerOn": 99999,
+    "re4TimerOff": 99999, "temp": 25.87, "tempRe": 255, "tempReCond": 2,
+    "tempReVal": 0, "tempAltCond": 2, "tempAltVal": 0, "hum": 54.03,
+    "humRe": 255, "humReCond": 0, "humReVal": 2, "humAltCond": 2,
+    "humAltVal": 0, "SHTUp": 1, "tempAvg": 25.8, "humAvg": 54.6,
+    "tempAvgPrev": 0, "humAvgPrev": 0, "soil1": 2735, "soil1AltCond": 2,
+    "soil1AltVal": 0, "soil1Up": 1, "soil2": 0, "soil2AltCond": 2,
+    "soil2AltVal": 0, "soil2Up": 1, "soil3": 0, "soil3AltCond": 2,
+    "soil3AltVal": 0, "soil3Up": 1, "soil4": 139, "soil4AltCond": 2,
+    "soil4AltVal": 0, "soil4Up": 1, "violet": 450, "indigo": 1025, "blue": 1448,
+    "cyan": 2912, "green": 3517, "yellow": 6408, "orange": 7688, "red": 5191,
+    "nir": 2170, "clear": 18000, "photo": 3248, "violetAvg": 420.2,
+    "indigoAvg": 907.6, "blueAvg": 1331.6, "cyanAvg": 2516.2, "greenAvg": 3439.1,
+    "yellowAvg": 5557.7, "orangeAvg": 7343.4, "redAvg": 4478.9, "nirAvg": 2179.7,
+    "clearAvg": 17538, "photoAvg": 3130.2, "violetAvgPrev": 0,
+    "indigoAvgPrev": 0, "blueAvgPrev": 0, "cyanAvgPrev": 0, "greenAvgPrev": 0,
+    "yellowAvgPrev": 0, "orangeAvgPrev": 0, "redAvgPrev": 0, "nirAvgPrev": 0,
+    "clearAvgPrev": 0, "photoAvgPrev": 0, "lightRe": 255, "lightReCond": 2,
+    "lightReVal": 0, "lightDur": 7894, "photoUp": 1, "specUp": 1,
+    "darkVal": 500, "atime": 29, "astep": 599, "again": 9, "repTimeEn": 0,
+    "repSendTime": 86340
+ }	
 
 app.get("/OTAcheck", (req, res) => {
 
@@ -69,6 +66,12 @@ app.get("/OTAcheck", (req, res) => {
         res.send(resp);
     })
     .catch(err => console.log(err));
+});
+
+app.get("/ch", (req, res) => { // Easy way to see changes or change dynamic doc.
+    const {key, val} = req.query;
+    allData[key] = val;
+    res.status(200).send("OK");
 });
 
 wss.on("connection", (ws, req) => { // Set all event handlers
@@ -101,94 +104,181 @@ const process = (CMD, clientID) => {
     if (CMD[0] == -1) return;
 
     const clientSocket = clients[clientID];
-    let json;
+    let json, val = CMD[1];
 
     if (clientSocket && clientSocket.readyState === webSocket.OPEN) {
 
         switch (CMDS[CMD[0]]) {
             case "GET_ALL":
             allData["id"] = `${CMD[2]}`;
-
             clientSocket.send(JSON.stringify(allData));
             break;
 
             case "CALIBRATE_TIME":
-                json = {"status":1, "id": `${CMD[2]}`};
-                clientSocket.send(JSON.stringify(json));
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            break;
+
+            case "RELAY_CTRL": {
+            let reNum = (val >> 4) & 0b1111;
+            let cmd = (val & 0b1111);
+            allData[`re${reNum}`] = cmd;
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            
+            break;
+
+            case "RELAY_TIMER": {
+            let reNum = (val >> 28) & 0b1111;
+            let start = (val >> 11) & 0x1FFFF;
+            let dur = (val & 0x7FF);
+
+            let end = (start + (dur * 60)) % 86400;
+
+            allData[`re${reNum}TimerEn`] = (start === 99999) ? 0 : 1;
+            allData[`re${reNum}TimerOn`] = start;
+            allData[`re${reNum}TimerOff`] = end;
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
 
             break;
 
+            case "ATTACH_RELAYS": {
+            let device = (val >> 4) & 0b1111;
+            let reNum = val % 0b1111;
+
+            switch (device) {
+                case 0: // Temp
+                allData.tempRe = (reNum == 4) ? 255 : reNum;
+                break;
+
+                case 1: // Hum
+                allData.humRe = (reNum == 4) ? 255 : reNum;
+                break;
+
+                case 2: // Light
+                allData.lightRe = (reNum == 4) ? 255 : reNum;
+                break;
+            }
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "SET_TEMPHUM": {
+                let sensor = (val >> 25) & 0b1;
+                let control = (val >> 24) & 0b1;
+                let condition = (val >> 16) & 0b11;
+                let value = val & 0xFFFF;
+                
+                if (sensor === 1) {
+                    
+                    if (control == 0) { // relay
+                        allData.tempReVal = (condition == 2) ? 0 : value;
+                        allData.tempReCond = condition;
+                    } else { // alt
+                        allData.tempAltVal = (condition == 2) ? 0 : value;
+                        allData.tempAltCond = condition;
+                    }
+    
+                } else {
+    
+                    if (control == 0) { // relay
+                        allData.humReVal = (condition == 2) ? 0 : value;
+                        allData.humReCond = condition;
+                    } else { // alt
+                        allData.humAltVal = (condition == 2) ? 0 : value;
+                        allData.humAltCond = condition;
+                    }
+                }
+            }
+            
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            break;
+
+            case "SET_SOIL": {
+            let num = (val >> 20) & 0b1111;
+            let cond = (val >> 16) & 0b111;
+            let v = (val & 0xFFFF);
+            allData[`soil${num}AltCond`] = cond;
+            allData[`soil${num}AltVal`] = (cond == 2) ? 0 : v;
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "SET_LIGHT": {
+            let type = (val >> 28) & 0b1;
+            let cond = (val >> 24) & 0b11;
+            let dark = (val >> 12) & 0x0FFF;
+            let photo = val & 0x0FFF;
+
+            switch (type) {
+                case 0: // Dark
+                allData.darkVal = dark;
+                break;
+
+                case 1: // photo
+                allData.lightReCond = cond;
+                allData.lightReVal = (cond == 2) ? 0 : photo;
+                break;
+            }
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "SET_SPEC_INTEGRATION_TIME": {
+            let ATIME = val & 0xFF;
+            let ASTEP = (val >> 8) & 0xFFFF;
+            allData.astep = ASTEP;
+            allData.atime = ATIME;
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "SET_SPEC_GAIN": {
+            allData.again = val;
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "CLEAR_AVERAGES": {
+            // No testing needed here.
+            json = {"status":1, "id": `${CMD[2]}`};
+            clientSocket.send(JSON.stringify(json));
+            }
+            break;
+
+            case "SEND_REPORT_SET_TIME": {
+            allData.repTimeEn = (val != 99999);
+            allData.repSendTime = val;
+            }
+            break;
+          
+
             case "GET_TRENDS":
-                json = {"id": `${CMD[2]}`,
-                    "temp": [21.16,21.29,0,0,0,0],
-                    "hum": [57.7,57.51,0,0,0,0],
-                    "clear": [15597,15630,0,0,0,0],
-                    "violet": [257,258,0,0,0,0],
-                    "indigo": [529,530,0,0,0,0],
-                    "blue": [945,947,0,0,0,0],
-                    "cyan": [1380,1382,0,0,0,0],
-                    "green": [2382,2385,0,0,0,0],
-                    "yellow": [4093,4109,0,0,0,0],
-                    "orange": [5644,5663,0,0,0,0],
-                    "red": [3853,3866,0,0,0,0],
-                    "nir": [1548,1550,0,0,0,0]};
+            json = {"id": `${CMD[2]}`,
+                "temp": [21.16,21.29,0,0,0,0],
+                "hum": [57.7,57.51,0,0,0,0],
+                "clear": [15597,15630,0,0,0,0],
+                "violet": [257,258,0,0,0,0],
+                "indigo": [529,530,0,0,0,0],
+                "blue": [945,947,0,0,0,0],
+                "cyan": [1380,1382,0,0,0,0],
+                "green": [2382,2385,0,0,0,0],
+                "yellow": [4093,4109,0,0,0,0],
+                "orange": [5644,5663,0,0,0,0],
+                "red": [3853,3866,0,0,0,0],
+                "nir": [1548,1550,0,0,0,0]};
 
             clientSocket.send(JSON.stringify(json));      
             break;
-
-            case "ATTACH_TEMP_RELAY":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempRe"] = CMD[1] == 4 ? 255 : CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                break;
-
-            case "SET_TEMP_RE_LWR_THAN":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempReCond"] = 0;
-                allData["tempReVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("LWR THAN");
-                break;
-
-            case "SET_TEMP_RE_GTR_THAN":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempReCond"] = 1;
-                allData["tempReVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("GTR THAN");
-                break;
-
-            case "SET_TEMP_RE_COND_NONE":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempReCond"] = 2;
-                allData["tempReVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("RE NONE");
-                break;
-
-            case "SET_TEMP_ALT_LWR_THAN":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempAltCond"] = 0;
-                allData["tempAltVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("LWR THAN");
-                break;
-
-            case "SET_TEMP_ALT_GTR_THAN":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempAltCond"] = 1;
-                allData["tempAltVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("GTR THAN");
-                break;
-
-            case "SET_TEMP_ALT_COND_NONE":
-                json = {"status": 1, "msg": "OK", "supp": 0, "id": `${CMD[2]}`};
-                allData["tempAltCond"] = 2;
-                allData["tempAltVal"] = CMD[1];
-                clientSocket.send(JSON.stringify(json));
-                console.log("RE NONE");
-                break;
         }
     }
 }
--- test.js
+++ test.js
@@ -1,9 +1,111 @@
 "use strict";
 
-const word = "ASpec";
+
+let encodeTH = (sensor, control, condition, value) => {
+    let ret = 0;
+    const shiftVals = {
+        'T': [1, 25], 'A': [1, 24], 'G': [1, 16], 'N': [2, 16]
+    };
+
+    if (shiftVals[sensor]) ret |= shiftVals[sensor][0] << shiftVals[sensor][1];
+    if (shiftVals[control]) ret |= shiftVals[control][0] << shiftVals[control][1];
+    if (shiftVals[condition]) ret |= shiftVals[condition][0] << shiftVals[condition][1];
+    if (value <= 65535) ret |= value;
+    return ret;
+}
+
+let decodeTH = (val) => {
+    let sensor = (val >> 25) & 0b1;
+    let control = (val >> 24) & 0b1;
+    let condition = (val >> 16) & 0b11;
+    let value = val & 0xFFFF;
+    console.log(`S-${sensor}, Ctl-${control}, Cnd-${condition}, V-${value}`);
+}
+
+let encodeREPeriph = (dev, renum) => {
+    let ret = 0;
+    if (dev != 0) ret |= (dev << 4);
+    if (renum != 0) ret |= renum;
+    return ret;
+}
+
+let decodeREPeriphe = (val) => {
+    let device = (val >> 4) & 0b1111;
+    let renum = val & 0b1111;
+    console.log(`D-${device} R-${renum}`)
+}
+
+let encodeRE = (num, cmd) => {
+    let ret = 0;
+    if (num != 0) ret |= (num << 4);
+    if (cmd != 0) ret |= cmd;
+    return ret;
+}
+
+let decodeRE = (val) => {
+    let num = (val >> 4) & 0b1111;
+    let cmd = val & 0b1111;
+    console.log(`N-${num} C-${cmd}`)
+}
+
+let encodeSO = (num, cond, val) => {
+    let ret = 0;
+    if (num != 0) ret |= (num << 20);
+    if (cond != 0) ret |= (cond << 16);
+    if (val != 0) ret |= (val);
+    return ret;
+}
+
+let decodeSOI = (val) => {
+    let num = (val >> 20) & 0b1111;
+    let cond = (val >> 16) & 0b1111;
+    let v = val & 0xFFFF;
+    console.log(`N-${num} C-${cond} V-${v} RAW-${val}`);
+}
+
+let encodeTM = (num, start, dur) => {
+    let ret = 0;
+    if (num != 0) ret |= (num << 28);
+    if (start != 0) ret |= (start << 11);
+    if (dur != 0) ret |= dur; 
+    return ret;
+}
+
+let decodeTM = (val) => {
+    let num = (val >> 28) & 0b1111;
+    let start = (val >> 11) & 0x1FFFF; 
+    let dur = val & 0x7FF; // 11 bits.
+    console.log(`N-${num} S-${start} D-${dur} Raw-${val}`);
+}
+
+let encodeLt = (tp, c, dk, ph) => {
+    let ret = 0;
+    if (tp != 0) ret |= (tp << 28);
+    if (c != 0) ret |= (c << 24)
+    if (dk != 0) ret |= (dk << 12);
+    if (ph != 0) ret |= ph;
+    return ret;
+}
+
+let decodeLt = (val) => {
+    let tp = (val >> 28) & 0b1;
+    let c = (val >> 24) & 0b11;
+    let dk = (val >> 12) & 0x0FFF;
+    let ph = val & 0x0FFF;
+    console.log(`T-${tp} C-${c} D-${dk} P-${ph} RAW-${val}`);
+}
+
+let ele = function(ID, RO) {
+    this.ID = ID;
+    this.RO = RO;
+    return this;
+}
+
+const a = new ele(ID = 55, RO = {'a': 74});
+
+console.log(a.ID);
 
 
 
 
 
-console.log(word[0]);
