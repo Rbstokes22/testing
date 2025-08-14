01a3431b09b11dcd979ce21f748f3cda7118becc
= <=>
2025-08-14 13:45:29 -0400

    Allowed log reverse order

--- test.js
+++ test.js
@@ -1,29 +1,8 @@
 "use strict";
 
-const max = 32767;
-const steps = max / 1000;
+let mystuff = "Hello;what;is;your;name";
 
-let toPerc = (intVal) => {
-    const val = Number((intVal / (steps * 10)).toFixed(1));
-    return val;
-}
+let stuff = mystuff.split(';').reverse();
 
-let toIntVal = (perc) => {
-    let reduction = perc * 10; // Put into steps 
-    return Math.floor(reduction * steps);
-}
-
-
-// for (let i = 0; i < 1001; i++) {
-//     const stPerc = i/10;
-//     const val = toIntVal(stPerc);
-//     const perc = toPerc(val);
-
-//     if (stPerc != perc) console.log(i);
-
-// }
-
-console.log(toIntVal(1));
-
-
+console.log(stuff);
 
